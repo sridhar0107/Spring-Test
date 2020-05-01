675566dc72d84248103051f057a91a9fe8c01384
sridhar0107 <64138326+sridhar0107@users.noreply.github.com>
2020-05-01 15:12:07 +0530

    Add files via upload

---
+++ Spring-Test/Test/src/main/java/com/capgemini/Test/Dao/DaoClass.java
@@ -0,0 +1,13 @@
+package com.capgemini.Test.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.Test.entity.Test;
+
+@Repository
+public interface DaoClass extends JpaRepository<Test,Integer>{
+	
+	
+
+}
---
+++ Spring-Test/Test/src/main/java/com/capgemini/Test/entity/Test.java
@@ -0,0 +1,57 @@
+package com.capgemini.Test.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Test")
+public class Test {
+
+	@Id
+	private int id;
+	private String title;
+	private String duration;
+	private int totalmarks;
+	private String starttime;
+	private String endtime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public int getTotalmarks() {
+		return totalmarks;
+	}
+	public void setTotalmarks(int totalmarks) {
+		this.totalmarks = totalmarks;
+	}
+	public String getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+	public String getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
+	
+
+	
+}
